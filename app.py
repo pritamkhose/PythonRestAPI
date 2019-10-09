df7ecf40dc62ee10f21c5514f12f28a3921eb840
pritamkhose <pritamkhose@gmaio.com>
2019-10-09 22:15:37 +0530

    update db url

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.secret_key = "secret key"
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/roytuts"
-app.config["MONGO_URI"] = "mongodb://**Username**:**Password**@cluster0-shard-00-00-qhmqk.mongodb.net:27017,cluster0-shard-00-01-qhmqk.mongodb.net:27017,cluster0-shard-00-02-qhmqk.mongodb.net:27017/python?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+app.config["MONGO_URI"] = "mongodb://pritam:pritam@cluster0-shard-00-00-qhmqk.mongodb.net:27017,cluster0-shard-00-01-qhmqk.mongodb.net:27017,cluster0-shard-00-02-qhmqk.mongodb.net:27017/python?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 mongo = PyMongo(app)
